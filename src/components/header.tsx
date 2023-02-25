db9952689a7587cad3e009e741b875d29ecf551b
kento <kento@toriwatari.bird>
2023-02-25 22:30:26 +0900

    ✨ Create Header Component

---
+++ src/components/header.tsx
@@ -0,0 +1,12 @@
+import styles from "../styles/header.module.css"
+
+const Header = () => {
+  return (
+    <header className={styles.header}>
+      <h1>Sample Page</h1>
+    </header>
+  )
+}
+
+export default Header
+
