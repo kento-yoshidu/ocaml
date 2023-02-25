5eab20418332bd0ea0cf33e933f9bba6d0702b35
kento <kento@toriwatari.bird>
2023-02-26 07:14:19 +0900

    ✨ Update header.tsx

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,10 +3,9 @@ import styles from "../styles/header.module.css"
 const Header = () => {
   return (
     <header className={styles.header}>
-      <h1>Sample Page</h1>
+      <h1>Sample Page 3</h1>
     </header>
   )
 }
 
 export default Header
-
