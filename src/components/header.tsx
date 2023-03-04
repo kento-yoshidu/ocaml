1395791d4bc8dc0ba681f63c101a56c49e9e093b
kento <kento@toriwatari.bird>
2023-03-04 14:02:50 +0900

    ✨ Update Header Component

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,7 +3,27 @@ import styles from "../styles/header.module.css"
 const Header = () => {
   return (
     <header className={styles.header}>
-      <h1>Sample Page 3</h1>
+      <h1 className={styles.title}>Sample Page 3</h1>
+
+      <h2 className={styles.subTitle}>
+        Sample Web Service
+      </h2>
+
+      <p className={styles.text}>
+        負荷分散
+      </p>
+
+      <p className={styles.text}>
+        自動デプロイ
+      </p>
+
+      <p className={styles.text}>
+        ロードバランシング
+      </p>
+
+      <p className={styles.text}>
+        マイクロサービス
+      </p>
     </header>
   )
 }
--- src/styles/header.module.css.d.ts
+++ src/styles/header.module.css.d.ts
@@ -1,5 +1,8 @@
 declare const styles: {
   readonly "header": string;
+  readonly "subTitle": string;
+  readonly "headerTitle": string;
+  readonly "headerText": string;
 };
 export = styles;
 
