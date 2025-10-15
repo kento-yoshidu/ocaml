1b9a39ea75981d5c34aa3b696a06a2f7461a59e1
kento-yoshidu <kento_yoshidu@yahoo.co.jp>
2025-10-15 20:13:26 +0900

    🦍 ABC029 A

---
+++ atcoder/abc0xx/abc029/a.ml
@@ -0,0 +1,11 @@
+(* https://atcoder.jp/contests/abc029/tasks/abc029_a *)
+
+let fn s =
+    s ^ "s"
+
+let () =
+    print_endline (fn "dog");
+    (* dogs *)
+
+    print_endline (fn "chokudai")
+    (* chokudais *)
