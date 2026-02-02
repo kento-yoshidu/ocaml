3135c779114ebdcc69919719868a116d1922ec73
kento-yoshidu <kento_yoshidu@yahoo.co.jp>
2026-02-02 19:09:57 +0900

    🌴 ABC443 A

---
+++ atcoder/abc4xx/abc443/a.ml
@@ -0,0 +1,14 @@
+(* https://atcoder.jp/contests/abc443/tasks/abc443_a *)
+
+let fn s =
+    s ^ "s"
+
+let () =
+    print_endline (fn "http");
+    (* https *)
+
+    print_endline (fn "append");
+    (* appends *)
+
+    print_endline (fn "beginner")
+    (* beginners *)
