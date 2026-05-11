430afeca5a0c3e056172c36804baaa62746c4ca9
kento-yoshidu <kento_yoshidu@yahoo.co.jp>
2026-05-11 21:32:46 +0900

    🐷 ABC457 A

---
+++ atcoder/abc4xx/abc457/a.ml
@@ -0,0 +1,14 @@
+(* https://atcoder.jp/contests/abc457/tasks/abc457_a *)
+
+let fn n a x =
+    a.(x-1)
+
+let () =
+    print_endline (string_of_int (fn 5 [| 1; 2; 3; 4; 5 |] 3));
+    (* 3 *)
+
+    print_endline (string_of_int (fn 10 [| 6; 6; 9; 6; 10; 5; 7; 2; 8; 2 |] 4));
+    (* 6 *)
+
+    print_endline (string_of_int (fn 10 [| 4; 4; 4; 3; 4; 2; 1; 1; 2; 1 |] 10))
+    (* 1 *)
